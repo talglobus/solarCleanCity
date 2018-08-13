45e4e2ab760ef36bc17baaeffca3625dabc1202a
Tal Globus <talglobus@gmail.com>
2018-08-14 00:43:58 +0800

    Fix views

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ let receiveRouter = require('./routes/receive');
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
